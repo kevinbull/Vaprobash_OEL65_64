4ce57cb8a0bb566423b353ddb9094457f3d2fbac
Kevin Bull <kevinbull@users.noreply.github.com>
2014-08-14 21:24:05 -0500

    Update base.sh

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,17 +3,23 @@
 echo ">>> Installing Base Packages"
 
 if [[ -z $1 ]]; then
-    github_url="https://raw.githubusercontent.com/fideloper/Vaprobash/master"
+    github_url="https://raw.githubusercontent.com/kevinbull/Vaprobash_OEL65_64/master"
 else
     github_url="$1"
 fi
 
-# Update
-sudo apt-get update
+# UTC        for Universal Coordinated Time
+# EST        for Eastern Standard Time
+# US/Central for American Central
+# US/Eastern for American Eastern
+server_timezone  = "US/Central"
 
-# Install base packages
-# -qq implies -y --force-yes
-sudo apt-get install -qq curl unzip git-core ack-grep
+# Update
+echo "Updating yum"
+sudo yum update
+
+echo "installing base packages"
+sudo yum install -y unzip curl-devel expat-devel gettext-devel openssl-devel zlib-devel git-core
 
 # Git Config and set Owner
 curl --silent -L $github_url/helpers/gitconfig > /home/vagrant/.gitconfig
@@ -25,18 +31,24 @@ git config --global http.postBuffer 65536000
 # Cache http credentials for one day while pull/push
 git config --global credential.helper 'cache --timeout=86400'
 
+echo "adding rpm repository"
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+
+# Set the server timezone
+echo "setting timezone to #{server_timezone}; ln -sf /usr/share/zoneinfo/#{server_timezone} /etc/localtime"
+
 
 echo ">>> Installing *.xip.io self-signed SSL"
 
-SSL_DIR="/etc/ssl/xip.io"
-DOMAIN="*.xip.io"
-PASSPHRASE="vaprobash"
+SSL_DIR="/etc/ssl/thelogue.dev"
+DOMAIN="*.thelogue.dev"
+PASSPHRASE="logue"
 
 SUBJ="
 C=US
-ST=Connecticut
-O=Vaprobash
-localityName=New Haven
+ST=Texas
+O=ClearC2
+localityName=Coppell
 commonName=$DOMAIN
 organizationalUnitName=
 emailAddress=
@@ -44,9 +56,9 @@ emailAddress=
 
 sudo mkdir -p "$SSL_DIR"
 
-sudo openssl genrsa -out "$SSL_DIR/xip.io.key" 1024
-sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.csr" -passin pass:$PASSPHRASE
-sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/xip.io.key" -out "$SSL_DIR/xip.io.crt"
+sudo openssl genrsa -out "$SSL_DIR/thelogue.dev.key" 1024
+sudo openssl req -new -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key "$SSL_DIR/thelogue.dev.key" -out "$SSL_DIR/thelogue.dev.csr" -passin pass:$PASSPHRASE
+sudo openssl x509 -req -days 365 -in "$SSL_DIR/xip.io.csr" -signkey "$SSL_DIR/thelogue.dev.key" -out "$SSL_DIR/thelogue.dev.crt"
 
 # Setting up Swap
 
@@ -58,24 +70,29 @@ if [[ ! -z $2 && ! $2 =~ false && $2 =~ ^[0-9]*$ ]]; then
     echo ">>> Setting up Swap ($2 MB)"
 
     # Create the Swap file
-    fallocate -l $2M /swapfile
+    sudo fallocate -l $2M /swapfile
 
     # Set the correct Swap permissions
-    chmod 600 /swapfile
+    sudo chmod 600 /swapfile
 
     # Setup Swap space
-    mkswap /swapfile
+    sudo mkswap /swapfile
 
     # Enable Swap space
-    swapon /swapfile
+    sudo swapon /swapfile
 
     # Make the Swap file permanent
-    echo "/swapfile   none    swap    sw    0   0" | tee -a /etc/fstab
+    echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
 
     # Add some swap settings:
     # vm.swappiness=10: Means that there wont be a Swap file until memory hits 90% useage
     # vm.vfs_cache_pressure=50: read http://rudd-o.com/linux-and-free-software/tales-from-responsivenessland-why-linux-feels-slow-and-how-to-fix-that
-    printf "vm.swappiness=10\nvm.vfs_cache_pressure=50" | tee -a /etc/sysctl.conf && sysctl -p
+    #printf "vm.swappiness=10\nvm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf && sysctl -p
+    
+    # For an explanation of swap files in Linux visit https://wiki.archlinux.org/index.php/swap
+    sudo sysctl vm.swappiness=10
+    sudo sysctl vm.vfs_cache_pressure=50
+    sudo sysctl vm.overcommit_memory=1
 
 fi
 
