571f4d32929dc24438e487bf5729be5e1998ea40
Kevin Bull <kevinbull@users.noreply.github.com>
2014-08-24 23:13:04 -0500

    Create oracle.sh

---
+++ scripts/oracle.sh
@@ -0,0 +1,18 @@
+echo "Installing Oracle"
+sudo rpm -Uvh /vagrant/oracle/oracle-instantclient12.1-basic-12.1.0.1.0-1.x86_64.rpm
+sudo rpm -Uvh /vagrant/oracle/oracle-instantclient12.1-devel-12.1.0.1.0-1.x86_64.rpm
+sudo rpm -Uvh /vagrant/oracle/oracle-instantclient12.1-sqlplus-12.1.0.1.0-1.x86_64.rpm
+
+echo "configuring oracle client"
+sudo chown -R vagrant:vagrant /etc/ld.so.conf.d
+sudo echo "/usr/lib/oracle/12.1/client64/lib" >> /etc/ld.so.conf.d/oracle.conf
+sudo chown -R root:root /etc/ld.so.conf.d
+
+sudo ldconfig
+
+export PATH=$PATH:/usr/lib/oracle/12.1/client64/bin
+
+echo "Installing Pecl OCI8"
+echo "instantclient,/usr/lib/oracle/12.1/client64/lib" | sudo pecl install oci8
+
+sudo sed -e "$a\extension=oci8.so"
